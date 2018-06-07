70e8382e80f1f9678ae6d0e12336841e51eea99a
makaimann <makaim@stanford.edu>
2018-06-06 21:52:07 -0700

    Change path

--- setenv.sh
+++ setenv.sh
@@ -1,22 +1,22 @@
 export PATH="$HOME/miniconda/bin:$PATH"
-export LLVM_CONFIG=$CGRAFLOW_PATH/llvm/bin/llvm-config
-export CLANG=$CGRAFLOW_PATH/llvm/bin/clang
-export PYTHONPATH=$PYTHONPATH:$CGRAFLOW_PATH/smt-switch
-export COREIR=$CGRAFLOW_PATH/coreir
+export LLVM_CONFIG=$CGRAFLOW_HOME/llvm/bin/llvm-config
+export CLANG=$CGRAFLOW_HOME/llvm/bin/clang
+export PYTHONPATH=$PYTHONPATH:$CGRAFLOW_HOME/smt-switch
+export COREIR=$CGRAFLOW_HOME/coreir
 export LD_LIBRARY_PATH=$COREIR/lib:$LD_LIBRARY_PATH
 
-export PYTHONPATH=$PYTHONPATH:$CGRAFLOW_PATH/smt_solvers/cvc4
-export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:$CGRAFLOW_PATH/smt_solvers/cvc4
+export PYTHONPATH=$PYTHONPATH:$CGRAFLOW_HOME/smt_solvers/cvc4
+export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:$CGRAFLOW_HOME/smt_solvers/cvc4
 
-export PATH=$PATH:$CGRAFLOW_PATH/smt_solvers/monosat
-export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:$CGRAFLOW_PATH/smt_solvers/monosat
+export PATH=$PATH:$CGRAFLOW_HOME/smt_solvers/monosat
+export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:$CGRAFLOW_HOME/smt_solvers/monosat
 
-export PATH=$PATH:$CGRAFLOW_PATH/smt_solvers/z3/bin/
-export PYTHONPATH=$PYTHONPATH:$CGRAFLOW_PATH/smt_solvers/z3/bin/python/
-export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:$CGRAFLOW_PATH/smt_solvers/z3/bin
+export PATH=$PATH:$CGRAFLOW_HOME/smt_solvers/z3/bin/
+export PYTHONPATH=$PYTHONPATH:$CGRAFLOW_HOME/smt_solvers/z3/bin/python/
+export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:$CGRAFLOW_HOME/smt_solvers/z3/bin
 
-export PYTHONPATH=$PYTHONPATH:$CGRAFLOW_PATH/smt_solvers/boolector
-export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:$CGRAFLOW_PATH/smt_solvers/boolector
+export PYTHONPATH=$PYTHONPATH:$CGRAFLOW_HOME/smt_solvers/boolector
+export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:$CGRAFLOW_HOME/smt_solvers/boolector
 
 export CC=gcc-4.9 
 export CXX=g++-4.9 
